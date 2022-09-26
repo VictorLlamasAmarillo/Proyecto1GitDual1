4770d23e49e22c3d8f9548d98a5f288ce4a97770
vllaama <victorllamasamarillo@gmail.com>
2022-09-26 13:40:06 +0200

    first

---
+++ src/everismpVLA/VLA.java
@@ -0,0 +1,9 @@
+package everismpVLA;
+
+public class VLA {
+
+	public static void main(String[] args) {
+	        System.out.println( "Hello World!!!!!" );
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Víctor
+ *
+ */
+module everismpVLA {
+}
