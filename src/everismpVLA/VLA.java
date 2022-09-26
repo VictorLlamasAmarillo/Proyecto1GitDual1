8d29eaa18fe7ec1aea0d9dcdb5774011bd9b41f5
vllaama <victorllamasamarillo@gmail.com>
2022-09-26 13:41:19 +0200

    second

--- src/everismpVLA/VLA.java
+++ src/everismpVLA/VLA.java
@@ -3,7 +3,7 @@ package everismpVLA;
 public class VLA {
 
 	public static void main(String[] args) {
-	        System.out.println( "Hello World!!!!!" );
+	        System.out.println( "Hello" );
 	}
 
 }
